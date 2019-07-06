1e2a838daa885c0cf95c7ce7b862d3c6e91cd91a
Julia Pereira <juliapcosta97@gmail.com>
2019-07-05 21:55:14 -0300

    Create Request Repository

---
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -0,0 +1,14 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.Request;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+
+	public List<Request> findAllOwnerId(Long id);
+}
